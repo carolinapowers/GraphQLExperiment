64437c92d571b4019123e6f600cf3dc1d2e3b16e
carolinapowers <carolinapoloni@gmail.com>
2018-01-19 11:02:25 -0500

    Seed users and projects

--- db/migrate/20180119152609_create_projects.rb
+++ db/migrate/20180119152609_create_projects.rb
@@ -1,6 +1,7 @@
 class CreateProjects < ActiveRecord::Migration[5.1]
   def change
     create_table :projects do |t|
+      t.string :title
       t.belongs_to :user, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 20180119152609) do
 
   create_table "projects", force: :cascade do |t|
+    t.string "title"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
