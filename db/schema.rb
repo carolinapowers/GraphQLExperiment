a8cc0fa5d961905814f02fb433329053cf5b1712
carolinapowers <carolinapoloni@gmail.com>
2018-01-19 10:29:07 -0500

    Add user projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119145046) do
+ActiveRecord::Schema.define(version: 20180119152609) do
+
+  create_table "projects", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
